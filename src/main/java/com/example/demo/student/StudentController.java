f65af6f623f96bfda37badb28cdad7e613d5ea2c
shajigholam <shajigholam@myseneca.ca>
2025-07-29 13:45:30 -0400

    initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    // holds a reference to service layer(contains the business logic)
+    private final StudentService studentService;
+
+    //Tells Spring to inject an instance of StudentService into this controller
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+}
